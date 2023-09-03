0380cdeb10be75f52ce7f3d8cfefd093f7357fb9
Rohan Basnet <rohan.basnet@southeasttech.edu>
2023-09-03 12:45:25 -0500

    Numbers 8-12 from the book

--- FutureValueCh02Ex1/Controllers/HomeController.cs
+++ FutureValueCh02Ex1/Controllers/HomeController.cs
@@ -1,14 +1,22 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-
+using FutureValueCh02Ex1.Models;
 namespace FutureValueCh02Ex1.Controllers
 {
     public class HomeController : Controller
     {
+        [HttpGet]
         public IActionResult Index()
         {
-            ViewBag.Name = "Mary";
-            ViewBag.FV = 99999.99;
+            ViewBag.FV = 0;
             return View();
         }
+
+        [HttpPost]
+        public IActionResult Index(FutureValueModel model)
+        {
+            ViewBag.FV = model.CalculateFutureValue();
+            return View(model);
+        }
+      
     }
 }
